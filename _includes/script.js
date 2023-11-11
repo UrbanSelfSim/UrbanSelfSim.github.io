8f23439ae832af96734b76ce521e783a76d37df2
UrbanSelfSim <149136328+UrbanSelfSim@users.noreply.github.com>
2023-11-11 14:26:13 +0800

    Create script.js

---
+++ _includes/script.js
@@ -0,0 +1,19 @@
+<script>
+// 获取应用菜单项
+const applicationsDropdown = document.getElementById('applications-dropdown');
+// 获取应用下拉菜单
+const dropdownContent = applicationsDropdown.querySelector('.dropdown-content');
+
+// 点击应用菜单项时显示/隐藏下拉菜单
+applicationsDropdown.addEventListener('click', function(event) {
+  event.stopPropagation(); // 阻止事件冒泡
+  dropdownContent.classList.toggle('show'); // 切换下拉菜单的显示状态
+});
+
+// 点击页面其他位置时隐藏下拉菜单
+document.addEventListener('click', function(event) {
+  if (event.target !== applicationsDropdown) {
+    dropdownContent.classList.remove('show'); // 隐藏下拉菜单
+  }
+});
+</script>
